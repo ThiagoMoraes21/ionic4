3ab54f7de7a7369e9fc7631caec6b0f68e5a7bda
Thiago Moraes <thiago4x70@gmail.com>
2019-07-26 09:55:31 -0300

    Add a cart page to show the selected items

--- angular_refresher/my-store/src/app/products.ts
+++ angular_refresher/my-store/src/app/products.ts
@@ -3,18 +3,18 @@ export const productsList = [
            name: "Phone XL",
            price: 799,
            description: "A large phone with one of the best screens",
-           img: 'http://lorempixel.com/600/300/technics'
+           img: 'https://source.unsplash.com/6wdRuK7bVTE'
          },
          {
            name: "Phone Mini",
            price: 699,
            description: "A great phone with one of the best cameras",
-           img: 'http://lorempixel.com/600/300/technics'
+           img: 'https://source.unsplash.com/A6JxK37IlPo'
          },
          {
            name: "Phone Standard",
            price: 299,
            description: "",
-           img: 'http://lorempixel.com/600/300/technics'
+           img: 'https://source.unsplash.com/tze1kKj7Lgg'
          }
        ];
